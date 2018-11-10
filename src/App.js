ddfd2100163bb99b0b0538692b5eed825c9db58f
Vesa Laakso <laakso.vesa@gmail.com>
2018-11-10 11:22:02 +0200

    Construct tree using hard-coded function

--- src/App.js
+++ src/App.js
@@ -3,22 +3,85 @@ import "./App.css";
 
 const stroke = { stroke: "black", "stroke-linecap": "round" };
 
-const branches = [
-  // Trunk
-  <path d="M200,400 200,300" stroke-width="10" {...stroke} />,
-  
-  // Left and right branches
-  <path d="M200,300 150,250" stroke-width="8" {...stroke} />,
-  <path d="M200,300 250,250" stroke-width="8" {...stroke} />,
-  
-  // 2 branches for left
-  <path d="M150,250 150,215" stroke-width="5" {...stroke} />,
-  <path d="M150,250 115,250" stroke-width="5" {...stroke} />,
-  
-  // 2 branches for right
-  <path d="M250,250 250,215" stroke-width="5" {...stroke} />,
-  <path d="M250,250 285,250" stroke-width="5" {...stroke} />,
-];
+function generateBranch({ startX, startY, level, xDiff, yDiff }) {
+  const strokeWidth = (() => {
+    switch (level) {
+      case 0:
+        return 10;
+      case 1:
+        return 8;
+      case 2:
+        return 5;
+      default:
+        throw new Error("too deep");
+    }
+  })();
+
+  const nextX = startX + xDiff;
+  const nextY = startY + yDiff;
+
+  return {
+    paths: (
+      <path
+        d={`M${startX},${startY} ${nextX},${nextY}`}
+        stroke-width={strokeWidth}
+        {...stroke}
+      />
+    ),
+    nextX,
+    nextY
+  };
+}
+
+function generateTree() {
+  return Array(3)
+    .fill(null)
+    .reduce((acc, next, level) => {
+      if (level === 0) {
+        acc.push(
+          generateBranch({
+            startX: 200,
+            startY: 400,
+            xDiff: 0,
+            yDiff: -100,
+            level
+          })
+        );
+      } else if (level === 1) {
+        const { nextX: startX, nextY: startY } = acc[acc.length - 1];
+        acc.push(
+          generateBranch({ startX, startY, xDiff: -50, yDiff: -50, level })
+        );
+        acc.push(
+          generateBranch({ startX, startY, xDiff: 50, yDiff: -50, level })
+        );
+      } else if (level === 2) {
+        const left = acc[acc.length - 2];
+        const right  = acc[acc.length -1];
+        {
+          let { nextX: startX, nextY: startY } = left;
+          acc.push(
+            generateBranch({ startX, startY, xDiff: 0, yDiff: -35, level })
+          );
+          acc.push(
+            generateBranch({ startX, startY, xDiff: -35, yDiff: 0, level })
+          );
+        }
+        {
+          let { nextX: startX, nextY: startY } = right;
+          acc.push(
+            generateBranch({ startX, startY, xDiff: 0, yDiff: -35, level })
+          );
+          acc.push(
+            generateBranch({ startX, startY, xDiff: 35, yDiff: 0, level })
+          );
+        }
+      }
+
+      return acc;
+    }, [])
+    .map(value => value.paths);
+}
 
 class App extends Component {
   render() {
@@ -32,7 +95,7 @@ class App extends Component {
           baseProfile="full"
           xmlns="http://www.w3.org/2000/svg"
         >
-          {branches}
+          {generateTree()}
         </svg>
       </div>
     );
