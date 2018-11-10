eab01087e1e31d673ac818fc9174534fb7d80100
Vesa Laakso <laakso.vesa@gmail.com>
2018-11-10 11:38:28 +0200

    Add some randomness to angles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 const stroke = { stroke: "black", "stroke-linecap": "round" };
 
-function generateBranch({ startX, startY, level, angle, length }) {
+function generateBranch({ startX, startY, level, angle, length, genRand }) {
   const strokeWidth = (() => {
     switch (level) {
       case 0:
@@ -17,8 +17,10 @@ function generateBranch({ startX, startY, level, angle, length }) {
     }
   })();
 
-  const xDiff = Math.cos(angle) * length;
-  const yDiff = Math.sin(angle) * length;
+  const random = genRand();
+
+  const xDiff = Math.cos(angle + random) * length;
+  const yDiff = Math.sin(angle + random) * length;
 
   const nextX = startX + xDiff;
   const nextY = startY + yDiff;
@@ -36,7 +38,7 @@ function generateBranch({ startX, startY, level, angle, length }) {
   };
 }
 
-function generateTree() {
+function generateTree(genRand) {
   return Array(3)
     .fill(null)
     .reduce((acc, next, level) => {
@@ -47,12 +49,13 @@ function generateTree() {
             startY: 400,
             angle: -Math.PI / 2,
             length: 100,
+            genRand,
             level
           })
         );
       } else if (level === 1) {
         const { nextX: startX, nextY: startY } = acc[acc.length - 1];
-        const common = { startX, startY, level, length: 75 };
+        const common = { genRand, startX, startY, level, length: 75 };
         acc.push(generateBranch({ ...common, angle: -((3 * Math.PI) / 4) }));
         acc.push(generateBranch({ ...common, angle: -(Math.PI / 4) }));
       } else if (level === 2) {
@@ -60,13 +63,13 @@ function generateTree() {
         const right = acc[acc.length - 1];
         {
           let { nextX: startX, nextY: startY } = left;
-          const common = { startX, startY, level, length: 50 };
-          acc.push(generateBranch({ ...common, angle: -(Math.PI) }));
+          const common = { genRand, startX, startY, level, length: 50 };
+          acc.push(generateBranch({ ...common, angle: -Math.PI }));
           acc.push(generateBranch({ ...common, angle: -(Math.PI / 2) }));
         }
         {
           let { nextX: startX, nextY: startY } = right;
-          const common = { startX, startY, level, length: 50 };
+          const common = { genRand, startX, startY, level, length: 50 };
           acc.push(generateBranch({ ...common, angle: -(Math.PI / 2) }));
           acc.push(generateBranch({ ...common, angle: 0 }));
         }
@@ -78,7 +81,17 @@ function generateTree() {
 }
 
 class App extends Component {
+  componentDidMount() {
+    this.interval = setInterval(() => this.forceUpdate(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
+    const genRand = () => Math.PI * ((Math.random() - 0.5) * (1 / 16));
+
     return (
       <div>
         <svg
@@ -89,7 +102,7 @@ class App extends Component {
           baseProfile="full"
           xmlns="http://www.w3.org/2000/svg"
         >
-          {generateTree()}
+          {generateTree(genRand)}
         </svg>
       </div>
     );
